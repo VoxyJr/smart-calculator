36da0ccb92296195e192ea62c75afb1f7c409768
VoxyJr <anxioushampongo@gmail.com>
2026-05-17 03:45:03 +0200

    Add math evaluator in Go

--- calc.go
+++ calc.go
@@ -2,13 +2,56 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"syscall/js"
 )
 
 func evaluate(this js.Value, args []js.Value) interface{} {
-	expr := args[0].String()
-	result := fmt.Sprintf("Got: %s", expr)
-	return result
+	expr := strings.ReplaceAll(args[0].String(), " ", "")
+	result, err := calc(expr)
+	if err != nil {
+		return "Error: " + err.Error()
+	}
+	return fmt.Sprintf("%g", result)
+}
+
+func calc(expr string) (float64, error) {
+	// Find + or - (lowest precedence, right to left)
+	for i := len(expr) - 1; i >= 0; i-- {
+		if expr[i] == '+' && i > 0 {
+			l, err := calc(expr[:i])
+			if err != nil { return 0, err }
+			r, err := calc(expr[i+1:])
+			if err != nil { return 0, err }
+			return l + r, nil
+		}
+		if expr[i] == '-' && i > 0 {
+			l, err := calc(expr[:i])
+			if err != nil { return 0, err }
+			r, err := calc(expr[i+1:])
+			if err != nil { return 0, err }
+			return l - r, nil
+		}
+	}
+	// Find * or /
+	for i := len(expr) - 1; i >= 0; i-- {
+		if expr[i] == '*' {
+			l, err := calc(expr[:i])
+			if err != nil { return 0, err }
+			r, err := calc(expr[i+1:])
+			if err != nil { return 0, err }
+			return l * r, nil
+		}
+		if expr[i] == '/' {
+			l, err := calc(expr[:i])
+			if err != nil { return 0, err }
+			r, err := calc(expr[i+1:])
+			if err != nil { return 0, err }
+			return l / r, nil
+		}
+	}
+	return strconv.ParseFloat(expr, 64)
 }
 
 func main() {
